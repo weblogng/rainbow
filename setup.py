f1f6019aefcadd8922e35b63b57d47e8b586bb9b
Stephen Kuenzli <skuenzli@weblogng.com>
2015-05-31 09:54:07 -0700

    Create a setup.py for rainbow package.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='rainbow-deploy',
+    version='0.1-dev',
+    description='simple blue-green deployment fabric api for file archives',
+    author='Stephen Kuenzli',
+    author_email='skuenzli@weblogng.com',
+    packages=['rainbow'])
