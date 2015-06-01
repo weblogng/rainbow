e3d707acdd0dc9ab3de2f7ca5e15003bbb4c9531
Stephen Kuenzli <skuenzli@weblogng.com>
2015-05-31 18:17:56 -0700

    Ensure remote path exists before trying to put artifact there.

--- rainbow/api.py
+++ rainbow/api.py
@@ -1,6 +1,6 @@
 from fabric.state import env
 #from fabric.api import (local, run, sudo, abort, task, cd, puts, require)
-from fabtools.require.files import (put)
+from fabtools.require.files import (directory, put)
 
 #alias fabric's env for simple unit-testing of the rainbow api
 fabric_env = env
@@ -8,6 +8,8 @@ fabric_env = env
 def deploy(artifact_name, remote_path):
     print "deploying artifact: {artifact_name} to {remote_path}"\
         .format(artifact_name=artifact_name, remote_path=remote_path)
+
+    directory(path=remote_path)
     put(local_path=artifact_name, remote_path=remote_path)
 
 def _roll_to_release(release):
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -2,18 +2,20 @@ import unittest
 
 from mock import patch
 
+@patch('fabtools.require.files.directory')
 @patch('fabtools.require.files.put')
 class DeployTestCase(unittest.TestCase):
 
-# in tests package
-
-    def test_deploy_puts_the_specified_artifact_to_the_remote_server(self, put):
+    def test_deploy_puts_the_specified_artifact_to_the_remote_server(self, put, directory):
         from rainbow import api
         artifact_name = "static-webapp-2015-04-18_20-24-25_UTC.35ccbf3.tar.gz"
         remote_path = "/opt/software"
         api.fabric_env.host_string = 'unit-test'
+        directory.return_value = None
         put.return_value = []
 
         api.deploy(artifact_name, remote_path)
+
+        directory.assert_called_with(path=remote_path)
 
         put.assert_called_with(local_path=artifact_name, remote_path=remote_path)
